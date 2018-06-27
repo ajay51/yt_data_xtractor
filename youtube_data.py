70055e4405d30fd4da3aa5d354035a7e2a4ea9eb
ajay51 <ajayanandverma91@gmail.com>
2018-06-27 03:49:57 -0700

    new notebook

--- youtube_data.py
+++ youtube_data.py
@@ -6,13 +6,13 @@ import pandas as pd
 import pprint 
 import matplotlib.pyplot as plt
 
-DEVELOPER_KEY = "AIzaSyAXUOtzLs_QzcgUhrq8WzD3EKKPpfcaALI"
+DEVELOPER_KEY = "AIzaSyAnvXIN2gyGsSnDyFwC0hKWL_5I6VeqM54"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
 def youtube_search(q, max_results=50,order="relevance", token=None, location=None, location_radius=None):
 
-    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
     search_response = youtube.search().list(
     q=q,
